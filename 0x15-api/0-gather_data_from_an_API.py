6293244899ef05d19add7e00cd4636bed75aa11a
kim kimtai <125466483+derrickkimtai@users.noreply.github.com>
2024-01-20 19:16:45 +0300

    chumba

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,38 +2,26 @@
 """
 this script fetches and dislays  the todo list of an employee
 """
-import json
-import urllib.request
+import requests
 import sys
 
-def get_employee(employee_id):
-    """fetch and parse the name of an employee"""
-    url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
-    with urllib.request.urlopen(url) as url:
-        data = json.loads(url.read().decode())
-
-        total_tasks = len(data)
-        completed_tasks = sum(task['completed'] for task in data)
-        return data, total_tasks, completed_tasks
-    
-def get_employee_name(employee_id):
-    """fetch and parse the name of an employee"""
-    url = f"https://jsonplaceholder.typicode.com/user/{employee_id}"
-    with urlib.request.urlopen(url) as url:
-        data = json.loads(url.read().decode())
-
-        return data['name']
-    
-def display_progress(employee_name, completed_tasks, total_tasks):
-    """Display the todo list progress of an employee"""
-    print(f"Employee {employee_name} is done with ({completed_tasks}/{total_tasks}):")
-    for task in data:
-        if task['completed']:
-            print("\t" + task['title'])
-
 if __name__ == "__main__":
-    employee_id = int(sys.argv[1])
-    task, total_tasks, completed_tasks = get_employee(employee_id)
-    employee_name = get_employee_name(employee_id)
-    display_progress(employee_name, completed_tasks, total_tasks)
-    
+    if len(sys.argv) >= 2 and sys.argv[1].isdigit():
+        user = sys.argv[1]
+        url = "https://jsonplaceholder.typicode.com/" + user
+        repo = requests.get(url)
+        name = repo.json().get("name")
+
+        url = "https://jsonplaceholder.typicode.com/" + user + "todos"
+        repo = requests.get(url)
+        done = len([todo for todo in repo.json() if todo.get("completed")])
+        total = len(repo.json())
+
+        first = True
+        for element in repo.json():
+            if first:
+                print("Employee {} is done with taks({}/{}:".
+                      format(name, done, total))
+                first = False
+            if element.get("completed"):
+                print("\t {}".format(element,get('title')))
