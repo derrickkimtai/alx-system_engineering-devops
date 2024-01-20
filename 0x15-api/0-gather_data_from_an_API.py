627629603969fbf0170b116bb8ac1ea3606816ff
kim kimtai <125466483+derrickkimtai@users.noreply.github.com>
2024-01-20 17:00:44 +0300

    changes made

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,4 +3,28 @@
 this script fetches and dislays  the todo list of an employee
 """
 import json
-import urli
+import urllib.request
+import sys
+
+def get_employee(employee_id):
+    """fetch and parse the name of an employee"""
+    url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
+    with urllib.request.urlopen(url) as url:
+        data = json.loads(url.read().decode())
+
+        total_tasks = len(data)
+        completed_tasks = sum(task['compeleted'] for task in data)
+        return data, total_tasks, completed_tasks
+    
+def display_progress(employee_name, completed_tasks, total_tasks):
+    """Display the todo list progress of an employee"""
+    print(f"Employee {employee_name} is done with ({completed_tasks}/{total_tasks}):")
+    for task in data:
+        if task['completed']:
+            print("\t" + task['title'])
+
+if __name__ == "__main__":
+    employee_id = int(sys.argv[1])
+    task, total_tasks, completed_tasks = get_employee(employee_id)
+    display_progress(employee_name, completed_tasks, total_tasks)
+    
